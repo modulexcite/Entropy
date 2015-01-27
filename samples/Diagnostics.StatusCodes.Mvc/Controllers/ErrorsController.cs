251829d6276844acd879adff0c8392a7da39bbeb
Chris Ross <Tratcher@Outlook.com>
2015-01-27 10:52:25 -0800

    StatusCodePages + MVC sample.

---
+++ samples/Diagnostics.StatusCodes.Mvc/Controllers/ErrorsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Diagnostics.StatusCodes.Mvc.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        [Route("errors/{id:int}")]
+        public IActionResult Index(int id)
+        {
+            return new ObjectResult("Error page, status code: " + id) { StatusCode = id };
+        }
+    }
+}
---
+++ samples/Diagnostics.StatusCodes.Mvc/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Diagnostics;
+using Microsoft.AspNet.Routing;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Diagnostics.StatusCodes.Mvc
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStatusCodePages(new StatusCodePagesOptions().WithReExecute("/errors/{0}"));
+
+            // Set up application services
+            app.UseServices(services =>
+            {
+                // Add MVC services to the services container
+                services.AddMvc();
+            });
+
+            // Add MVC to the request pipeline
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("ActionAsMethod", "{controller}/{action}",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
